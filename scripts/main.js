cc6e6373d95e39c7614959314243308f0a321427
Scepfari <63742921+Scepfari@users.noreply.github.com>
2022-05-29 15:37:49 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+Hooks.once('diceSoNiceInit', (dice3d) => {
+	
+	dice3d.addSystem({id: "space1889", name: "SPACE 1889 Templer"}, "default"); 
+	
+	dice3d.addDicePreset({
+	  type: "dc",
+	  modelFile: "modules/more-coins-for-dsn/model/templer_dc.glb",
+	  system: "space1889"
+	});
+
+});
