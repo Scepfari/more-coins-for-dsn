af927de77be044e7a41c36516edf7a1ec3e4a813
Scepfari <63742921+Scepfari@users.noreply.github.com>
2022-06-17 18:33:21 +0200

    add Space Gear

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,19 @@
 Hooks.once('diceSoNiceReady', (dice3d) => {
 	
-	dice3d.addSystem({id: "space1889", name: "SPACE 1889 Templer"}, "default"); 
+	dice3d.addSystem({id: "templerSpace1889", name: "Templer"}, "default"); 
 	
 	dice3d.addDicePreset({
 	  type: "dc",
 	  modelFile: "modules/more-coins-for-dsn/model/templer_dc.glb",
-	  system: "space1889"
+	  system: "templerSpace1889"
+	});
+
+	dice3d.addSystem({id: "gearSpace1889", name: "SPACE 1889 Gear"}, "default"); 
+	
+	dice3d.addDicePreset({
+	  type: "dc",
+	  modelFile: "modules/more-coins-for-dsn/model/dc_gear_space1889.glb",
+	  system: "gearSpace1889"
 	});
 
 });
